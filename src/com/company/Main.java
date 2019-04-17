a6f571a3b6e77a885838a39210c997dcd96d2c52
qwaukulous <qwauknation@yahoo.com>
2019-04-17 15:54:04 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	System.out.print("Cost \t \t Quantity \t \t Total \n $1,000.00 \t \t 4 \t \t $4,000.00 \n $   50.00 \t \t 8 \t \t   $400.00 \n ___________________________________ \n TOTAL: \t \t12 \t \t $4,400.00");
+    }
+}
